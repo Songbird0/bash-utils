9467adbb3f58e90c03542566a75654896277e193
Defranceschi Anthony <chaacygg@gmail.com>
2016-12-06 10:51:19 +0100

    ajout
    Ajout d'un script de configuration pour établir un environnement écrit en français. Fix également les erreurs régulièrement rencontrées avec locales dans les versions minimales des Debian.

---
+++ debian-config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Configuration d'une distribution Debian pour obtenir un environnement
+# rédigé en français et encodé en UTF-8.
+
+sudo apt-get update && sudo apt-get dist-upgrade
+echo "Sélection l'encodage de la langue: "
+sudo dpkg-reconfigure locales
+sudo apt-get install manpages-fr manpages-fr-dev manpages-fr-extra
+sudo apt-get install man-db
