49629fa39c0160d013bf5da5c74e4fbb7c813856
Songbird0 <Songbird0@users.noreply.github.com>
2017-07-07 18:21:05 +0200

    Update debian-config.sh
    Ajout d'utils dans le script d'installation.
    
    Ajout d'une version de Python 3 (dernière release lors de ce push).

--- debian-config.sh
+++ debian-config.sh
@@ -6,7 +6,13 @@
 sudo apt-get update && sudo apt-get dist-upgrade
 echo "Sélection de l'encodage de la langue: "
 sudo dpkg-reconfigure locales
-#Installation de quelques paquets essentiels
-#Il ne s'agit plus simplement d'installer les paquets pour un environnement configuré en français,
-#mais aussi des paquets utiles au quotidien et dont l'installation manuelle est, en générale, contre-productive.
-sudo apt-get install manpages-fr manpages-fr-dev manpages-fr-extra man-db tree git dnsutils bzip2
+# Installation de quelques paquets essentiels
+# Il ne s'agit plus simplement d'installer les paquets pour un environnement configuré en français,
+# mais aussi des paquets utiles au quotidien et dont l'installation manuelle est, en générale, contre-productive.
+sudo apt-get install manpages-fr manpages-fr-dev manpages-fr-extra man-db tree git dnsutils bzip2 gcc make
+echo "Installation de Python 3.6.1..."
+wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
+tar xvzf Python-3.6.1.tgz
+cd Python-3.6.1/
+./configure; make; make test; make install
+which python3
