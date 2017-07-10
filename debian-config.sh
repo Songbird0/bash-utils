8c13adeb07207fef16aa57e9d178e7650df0b9c3
Songbird0 <Songbird0@users.noreply.github.com>
2017-07-11 00:51:48 +0200

    Mise à jour de l'installateur

--- debian-config.sh
+++ debian-config.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+. python3_installer.sh
+. java_installer.sh
+
 # Configuration d'une distribution Debian pour obtenir un environnement
 # rédigé en français et encodé en UTF-8.
-
+if [ "$(whoami)" != 'root' ]
+then
+    echo "You're not logged in as user root. Please run this script with root."
+    exit 0
+fi
 sudo apt-get update && sudo apt-get dist-upgrade
-echo "Sélection de l'encodage de la langue: "
-sudo dpkg-reconfigure locales
+if [ "$LANG" != "fr_FR.UTF-8" ]
+then
+    echo "Sélection de l'encodage de la langue: "
+    dpkg-reconfigure locales
+else
+    echo 'La langue principale du système est le Français.'
+fi
 # Installation de quelques paquets essentiels
 # Il ne s'agit plus simplement d'installer les paquets pour un environnement configuré en français,
 # mais aussi des paquets utiles au quotidien et dont l'installation manuelle est, en générale, contre-productive.
 sudo apt-get install manpages-fr manpages-fr-dev manpages-fr-extra man-db tree git dnsutils bzip2 gcc make
-echo "Installation de Python 3.6.1..."
-wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
-tar xvzf Python-3.6.1.tgz
-cd Python-3.6.1/
-./configure; make; make test; make install
-which python3
+( install_python3 )
+
+# Les arguments peuvent être modifiés au besoin
+install_java https://www.dropbox.com/s/uf81xyw3rf9ct4z/jdk-8u112-linux-x64.tar.gz?dl=1 jdk1.8.0_112
